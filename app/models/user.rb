93c553cb10905999fa38a71f278d907fabd3daa7
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-10-07 14:58:07 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname,:first_name,:last_name,:first_name_kana,:last_name_kana, presence: true
+
+  # validates :birthday, numericality: { other_than: "--","can't be blank" }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "は英字と数字の両方を含めて設定して下さい"
 end
