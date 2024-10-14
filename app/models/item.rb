030a0ef3598b711d32f1a7ed964607656b734dd4
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-10-14 16:25:36 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,9 @@ class Item < ApplicationRecord
   belongs_to :Shipping_time
   belongs_to :user
 
+  validates :name, :item_comment, :price, presence: true
+
+  # 選択項目が「---」の時は保存できないようにする
+  validates :category_id, :item_status_id, :shipping_time_id, :shipping_fee_id,
+            :prefecture_id, numericality: {other_than: 1 }
 end
