715258033b66025cef8830bb9599c637cb23aa90
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-10-22 15:47:29 +0900

    itemモデル単体テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,58 +3,76 @@ RSpec.describe Item, type: :model do
   
   before do
     @item = FactoryBot.build(:item)
+    @user = FactoryBot.build(:user)
   end
   
   describe "商品出品機能" do
     context "商品が出品できるとき" do
       it "必要な情報が全て存在すれば出品できる" do
-      end
-      it "ログイン状態の場合に商品出品ページへ遷移できる" do
-      end
-      it "カテゴリーは初期値に---が設定されている" do
-      end
-      it "カテゴリーは11項目が表示される" do
-      end
-      it "商品の状態は初期値に---が設定されている" do
-      end
-      it "商品状態は7項目が表示される" do
-      end
-      it "発送元の地域は---が初期値に設定されている" do
-      end
-      it "発送元の地域は48項目が表示される" do
-      end
-      it "発送までの日数は初期値に---が設定されている" do
-      end
-      it "発送までの日数は4項目が表示される" do
+        expect(@item).to be_valid
       end
     end
 
     context "商品が出品できないとき" do
-      it "ログアウト状態では商品出品ページへ遷移できない" do
-      end 
       it "商品画像が１枚付いていなければ出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it "商品名が無ければ出品できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it "商品説明が無ければ出品できない"do
+        @item.item_comment = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item comment can't be blank")
       end
       it "カテゴリー情報が選択されていないと出品できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it "商品状態が選択されていないと出品できない" do
+        @item.item_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank")
       end
       it "配送料の負担が選択されていないと出品できない" do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
       it "発送元の地域が選択されていないと出品できない" do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it "発送までの日数が選択されていないと出品できない" do
+        @item.shipping_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping time can't be blank")
       end
       it "価格が無ければ出品できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not a number", "Price is invalid")
       end
       it "価格が300円未満では出品できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it "価格が9999999円より高いと出品できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-      it "価格が半角出ないと出品できない" do
+      it "価格が半角でなければ出品できない" do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
