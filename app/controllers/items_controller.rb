6ee66a6d207fc798c13c9cc2c93fe85172bafcbb
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-11-08 18:01:58 +0900

    レビュー指摘事項修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
   before_action :matched_user_id, only: [:edit, :update]
+  before_action :move_to_session, only: [:new]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -37,12 +37,6 @@ class ItemsController < ApplicationController
 
   private
 
-  def move_to_index
-    return if user_signed_in?
-
-    redirect_to action: :index
-  end
-
   def item_params
     params.require(:item).permit(:image, :name, :item_comment, :category_id, :item_status_id, :shipping_fee_id, :prefecture_id,
                                  :shipping_time_id, :price).merge(user_id: current_user.id)
@@ -50,8 +44,19 @@ class ItemsController < ApplicationController
 
   def matched_user_id
     @item = Item.find(params[:id])
-    return if current_user.id == @item.user_id
+    if user_signed_in?
+      return if current_user.id == @item.user_id
 
-    redirect_to root_path
+      redirect_to root_path
+
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_session
+    return if user_signed_in?
+
+    redirect_to new_user_session_path
   end
 end
