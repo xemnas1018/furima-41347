febf0516f23de79efd94f6ab15afc4a0675da130
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-11-13 16:54:10 +0900

    Fromオブジェクト作成

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,4 +1,18 @@
 class HistoriesController < ApplicationController
   def index
   end
-end
+
+  def new 
+    @history_shipping = HisstoryShipping.new
+  end
+
+  def create
+    @history_shipping = HisstoryShipping.new(hisstory_params)
+  end
+
+  private
+
+  def hisstory_params
+    params.require(:hisstory_shipping).permit(:postal_code, :address, :phone_num, :prefecture_id, :building_name, :house_num).merge(:user_id,current_user.id)
+
+  end
