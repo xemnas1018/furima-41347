1e5daf1f06df6089feb978bc908f5bbab8eb9b75
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-11-13 14:42:34 +0900

    テーブル作成

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,2 +1,4 @@
 class Shipping < ApplicationRecord
+  belongs_to :history
+  belongs_to :prefecture
 end
--- db/migrate/20241113051107_create_shippings.rb
+++
@@ -1,8 +0,0 @@
-class CreateShippings < ActiveRecord::Migration[7.0]
-  def change
-    create_table :shippings do |t|
-
-      t.timestamps
-    end
-  end
-end
