15f9fd264e62e36705e7a66523b1881d34b5ea2e
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-11-14 14:36:33 +0900

    売却済み商品購入ページ遷移防止機能実装

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,16 +2,16 @@ class HistoriesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create] 
   before_action :mismatch_user,only: [:index, :create]
+  before_action :sold_check, only: [:index]
+  before_action :set_history_shipping, only:[:index] 
 
   def index
-
-    @history_shipping = HistoryShipping.new
+    #@history_shipping = HistoryShipping.new
   end
 
 
   def create
     @history_shipping = HistoryShipping.new(history_params)
-    #binding.pry
     if @history_shipping.valid?
       @history_shipping.save
       redirect_to root_path
@@ -29,10 +29,21 @@ class HistoriesController < ApplicationController
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def set_history_shipping
+    @history_shipping = HistoryShipping.new
+  end
   
   def mismatch_user
     return unless current_user.id == @item.user_id
     redirect_to root_path
   end
+
+  def sold_check
+    @history = History.find(params[:item_id])
+    return if  @history.item_id != @item.id
+    redirect_to root_path
+  end
+
 end
 
