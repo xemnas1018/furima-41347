b7d1c39eb1b1f1f663bd88230d7443edd9b4fc93
xemnas1018 <yakusokunoomamori@hotmail.co.jp>
2024-11-14 17:56:08 +0900

    商品購入状況によるリンク表示変更機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :item_find_instance, only: [:edit, :update, :show, :destroy]
   before_action :matched_user_id, only: [:edit, :update, :destroy]
+  before_action :sold_check, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
-    #@history = History.new
-    #@histories = @item.histories.include(:user) 
-    
   end
 
   def new
@@ -53,6 +51,12 @@ class ItemsController < ApplicationController
     return if current_user.id == @item.user_id
 
     redirect_to root_path
+  end
+
+  def sold_check
+    if @item.history.present?
+      redirect_to root_path
+    end
   end
 
   def item_find_instance
